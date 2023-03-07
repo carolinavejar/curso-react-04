fec33df7c27a4530c7eb5cc6a54a5db54aadad2a
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-06 23:23:12 -0300

    Pruebas en el componente GifGridItem

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 export const GifItem = ( { title, url } ) => {
     return (
     <div className="card">
+        <p>{title}</p>
         <img src={url} alt={title} />
     </div>
   )
--- tests/components/GifItem.test.jsx
+++ tests/components/GifItem.test.jsx
@@ -1,13 +1,33 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en GifItem', ()=> {
+    
+    const title = "Titulo ...";
+    const url = "http://";
+
     test('Debe hacer mach con el snapshot', ()=> {
-        const title = "Titulo ...";
-        const url = "http://";
+        
         const { container } = render
         (< GifItem title= {title} url={ url} />);
 
         expect (container).toMatchSnapshot();
+    });
+
+    test('Debe mostrar URL y ALT indicado', ()=> {
+        render(< GifItem title= {title} url={ url} />);
+
+        // expect (screen.getByRole('img').src).toBe(url);
+        // expect (screen.getByRole('img').alt).toBe(title);
+
+        const { src, alt } = screen.getByRole('img');
+        expect(alt).toBe(title);
+        expect(src).toBe(url);
+    });
+
+    test('Debe mostrar el titulo del componente', ()=> {
+        render(< GifItem title= {title} url={ url} />);
+
+        expect(screen.getByText(title)).toBeTruthy;
     });
 })
