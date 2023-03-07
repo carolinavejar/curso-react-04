7c7c31cbb9567d75ed6afb780319289dd56f0e57
Carolina Vejar <caro.vejarc@gmail.com>
2023-03-07 17:57:49 -0300

    Hacer mock completo de un custom hook

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,17 +1,37 @@
 import { screen, render } from "@testing-library/react";
 import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en GifGrid', ()=> {
-
-    const category = 'Kilie'
+        const category = 'Kilie'
 
     test('Debe mostrar el loading inicialmente', ()=> {
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true
+        })
         render(< GifGrid category={category}  />);
         expect (screen.getByText('Cargando ...'));
         expect (screen.getByText(category))
     });
 
     test('Debe mostrar items cuando se cargan las imagenes useFetchGifs', ()=> {
+        
+        const gifs = [{
+            id: '123',
+            title: 'Unapologetic',
+            url: 'http://'
+        }]
+        
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
 
+        render(< GifGrid category={category}  />);
+
+        expect (screen.getAllByRole('img').length).toBe(1);
     });
 })
